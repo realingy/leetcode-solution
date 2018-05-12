c52d2e06c81d241863fcd50fdd80b1cc99ff19b4
realingy <754037927@qq.com>
2018-05-12 19:43:49 +0800

    add leet118

---
+++ array/leet118.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file leet118.cpp
+ * @brief  
+ * @author  
+ * @version 1.0
+ * @date 2018年05月12日 19时19分05秒
+ */
+
+//Pascal's Triangle
+//
+
+#include <vector>
+
+vector<vector<int>> pascal(int row)
+{
+    vector<vector<int>> ret;
+    
+    if(row <= 0) return ret;
+    ret.assign(row, vector<int>(1));
+    for(int i = 0; i < row; i++)
+    {
+        ret[i][0] = 1;
+        if(0 == i) continue;
+        for(int j = 1; j < i; j++)
+        {
+            ret[i].push_back(ret[i-1][j-1] + ret[i-1][j]);
+        }
+        ret[i].push_back(1);
+    }
+
+    return ret;
+}
+
