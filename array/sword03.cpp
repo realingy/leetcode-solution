370777aedc7e43a3f609454db64e89c043a931f3
realingy <754037927@qq.com>
2019-05-17 08:52:21 +0800

    [changelog] add project

---
+++ array/sword03.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+//查找数字
+bool FindNumber(int *array, int row, int column, int target)
+{
+    bool ret = false;
+
+    if(array != NULL && row >0 && column > 0) {
+        int r = 0;
+        int c = column - 1;
+        while(r < row && c >= 0)
+        {
+            if(array[r * column + c] == target) {
+                ret = true;
+                break;
+            }
+            else if(array[r * column + c] > target) {
+                --c;
+            }
+            else {
+                ++r;
+            }
+        }
+    }
+
+    return ret;
+}
+
+int main()
+{
+
+}
