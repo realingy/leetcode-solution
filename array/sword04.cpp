b00a4ff0e98a6823c85b32756b40462296fcf9cb
realingy <754037927@qq.com>
2019-05-20 14:24:42 +0800

    [changelog] add project

---
+++ array/sword04.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file sword04.cpp
+ * @brief  
+ * @author  
+ * @version 1.0
+ * @date 2019年05月17日 09时45分35秒
+ */
+
+//替换空格
+void ReplaceBlank(char string[], int len)
+{
+    if(string == NULL && len <= 0)
+        return;
+
+    int originalLength = 0;
+    int numberOfBlank = 0;
+    int i = 0;
+
+    while(string[i] != '\0')
+    {
+        ++originalLength;
+
+        if(string[i] == ' ')
+            ++numberOfBlank;
+
+        ++i;
+    }
+
+    int newLength = originalLength + numberOfBlank * 2;
+    if(newLength > len)
+        return;
+
+    int indexOfOriginal = originalLength;
+    int indexOfNew = newLength;
+}
+
+int main()
+{
+
+    return 0;
+}
+
