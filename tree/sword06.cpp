2824836e39f6f9e1aa1879d3bdf9eac474cafbf9
realingy <754037927@qq.com>
2019-05-23 16:51:48 +0800

    [changelog] add file

---
+++ tree/sword06.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct BinaryTreeNode
+{
+    int m_nValue;
+    BinaryTreeNode * m_pLeft;
+    BinaryTreeNode * m_pRight;
+};
+
+BinaryTreeNode * Construct(int *preorder, int * inorder, int length)
+{
+
+}
+
+
+
+
