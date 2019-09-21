364bd00b22063ccd1e7a232794aa08efbc6748c0
realingy <754037927@qq.com>
2019-09-21 18:04:12 +0800

    commit 1.twosum

--- leetcode-python/1.twonum.py
+++ leetcode-python/1.twonum.py
@@ -9,14 +9,12 @@ class Solution:
             if i == len(nums) - 1:
                 return "No solution here!"
             r = target - nums[i]
-            # Can't use a num twice
             num_follow = nums[i + 1:]
             if r in num_follow:
                 return [i, num_follow.index(r) + i + 1]
             i = i + 1
 
 if __name__ == "__main__":
-    # print("hello python")
     s = Solution()
     nums = [2, 7, 8, 9]
     indexs = s.twoSum(nums, 9)
