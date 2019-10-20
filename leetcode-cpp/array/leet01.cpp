058d69707ffd19682d18f6a96d7594d8c5f9068b
realingy <754037927@qq.com>
2019-10-20 11:21:44 +0800

    20191020

--- leetcode-cpp/array/leet01.cpp
+++ leetcode-cpp/array/leet01.cpp
@@ -1,4 +1,7 @@
-#include "header.h"
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
 
 class Solution {
 public:
@@ -32,3 +35,22 @@ public:
 
 };
 
+#if 0
+int main()
+{
+	Solution S;
+
+	vector<int> array = { 2, 7, 8, 9 };
+	vector<int> res1 = S.twoSum(array, 9);
+	vector<int> res2 = S.twoSum2(array, 9);
+
+	for (int i = 0; i < res1.size(); i++) {
+		cout << res1[i] << " ";
+	}
+
+	cout << '\n';
+
+	for (auto i : res2)
+		cout << i << ' ';
+}
+#endif
---
+++ leetcode-cpp/array/leet53.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	//动态规划
+	int maxSubArray(int* array, int len)
+	{
+		int res = INT_MIN;
+		int sum = 0;
+		for (size_t i = 0; i < len; i++)
+		{
+			int num = array[i];
+			sum = max(num, sum + num);
+			res = max(sum, res);
+		}
+		return res;
+	}
+	//分治法
+};
+
+#if 1
+int main()
+{
+	Solution S;
+	int array[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+	int len = sizeof(array) / sizeof(int);
+	cout << "max = " << S.maxSubArray(array, len);
+}
+#endif
+
