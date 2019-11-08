c3c2464a551ea9f409e4163958282d2a5c6440af
realingy <754037927@qq.com>
2019-11-08 16:48:17 +0800

    分治法

--- leetcode-cpp/array/leet53.cpp
+++ leetcode-cpp/array/leet53.cpp
@@ -20,7 +20,34 @@ public:
 		}
 		return res;
 	}
+
 	//·ÖÖÎ·¨
+	int maxSubArray2(int* array, int len)
+	{
+		return helper(array, 0, len-1);
+	}
+
+	int helper(int* a, int left, int right)
+	{
+		if (left >= right)
+			return a[left];
+
+		int mid = left + (right - left) / 2;
+		int maxl = helper(a, left, mid - 1);
+		int maxr = helper(a, mid+1, right);
+		int mmax = a[mid], t = mmax;
+		for (int i = mid-1; i >= left; i--) {
+			t += a[i];
+			mmax = max(mmax, t);
+		}
+		t = mmax;
+		for (int i = mid + 1; i <= right; i++) {
+			t += a[i];
+			mmax = max(mmax, t);
+		}
+		return max(mmax, max(maxl, maxr));
+	}
+
 };
 
 #ifdef S53
@@ -29,7 +56,7 @@ int main()
 	Solution S;
 	int array[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 	int len = sizeof(array) / sizeof(int);
-	cout << "max = " << S.maxSubArray(array, len);
+	cout << "max = " << S.maxSubArray2(array, len);
 }
 #endif
 
