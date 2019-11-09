7e9b81af1ee935df5462261b32143bb82bf05174
realingy <754037927@qq.com>
2019-11-09 18:08:59 +0800

    leetcode007

---
+++ leetcode-cpp/math/leet007.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include "switch.h"
+
+using namespace std;
+class Solution
+{
+public:
+	int atoi(string str)
+	{
+		// return -200;
+	}
+
+};
+
+#ifdef S007 
+int main()
+{
+	Solution S;
+	string s = "-200";
+	cout << "int: " << S.atoi(s) << "\n";
+}
+#endif
