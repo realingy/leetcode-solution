109ec3323a9bc542513f2fb19ef0ae9692de145f
realingy <754037927@qq.com>
2023-11-28 10:13:32 +0800

    update 20231127

--- leetcode-python/LeetArray.py
+++ leetcode-python/LeetArray.py
@@ -40,6 +40,53 @@ class LeetArray:
 
         return res
 
+    # leetcode485 最大连续1的个数
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        l = r = 0
+        res = 0
+        while l < len(nums) and r < len(nums):
+            while l < len(nums) and 1 != nums[l]:
+                l += 1
+            r = l
+            while r < len(nums) and 1 == nums[r]:
+                r += 1
+            res = max(res, r - l)
+            l = r
+        return res
+
+    # leetcode1984 学生分数的最小差值
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        # 排序
+        new_nums = sorted(nums)
+        n = len(new_nums)
+        res = inf
+        for i in range (0, n - k + 1):
+            res = min(res, new_nums[i+k-1]-new_nums[i])
+        return res
+
+    def takeCharacters(self, s: str, k: int) -> int:
+      n = len(s)
+      if(n < 3 * k):
+        return -1
+      j = n
+      cnt = Counter()
+      while cnt['a'] <  k or cnt['b'] < k or cnt['c'] < k:
+        if(j == 0): 
+          return -1
+        j -= 1
+        cnt[s[j]] += 1
+      
+      res = n - j
+      for i, ch in enumerate(s):
+        cnt[ch] += 1
+        while(j < n and cnt[s[j]] > k):
+          cnt[s[j]] -= 1
+          j += 1
+        res = min(res, i + 1 + n - j)
+        if j == n:
+          break
+      return res
+
 s = 'abcddf'
 la = LeetArray()
 len = la.countGoodSubstrings(s)
